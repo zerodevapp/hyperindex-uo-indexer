faef133115c590c9f4285d569682849041a32ef3
Sahil Vasava <sahilvasava@gmail.com>
2025-07-01 20:22:08 +0800

    test: update test

--- test/Test.ts
+++ test/Test.ts
@@ -1,38 +1,44 @@
 import assert from "assert";
 import { 
   TestHelpers,
-  EntryPoint_AccountDeployed
+  UserOperation
 } from "generated";
-const { MockDb, EntryPoint } = TestHelpers;
+const { MockDb, EntryPointV07 } = TestHelpers;
 
-describe("EntryPoint contract AccountDeployed event tests", () => {
+describe("EntryPoint contract UserOperation event tests", () => {
   // Create mock db
   const mockDb = MockDb.createMockDb();
 
-  // Creating mock for EntryPoint contract AccountDeployed event
-  const event = EntryPoint.AccountDeployed.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
+  // Creating mock for EntryPoint contract UserOperation event
+  const event = EntryPointV07.UserOperationEvent.createMockEvent({/* It mocks event fields with default values. You can overwrite them if you need */});
 
-  it("EntryPoint_AccountDeployed is created correctly", async () => {
+  it("UserOperation is created correctly", async () => {
     // Processing the event
-    const mockDbUpdated = await EntryPoint.AccountDeployed.processEvent({
+    const mockDbUpdated = await EntryPointV07.UserOperationEvent.processEvent({
       event,
       mockDb,
     });
 
     // Getting the actual entity from the mock database
-    let actualEntryPointAccountDeployed = mockDbUpdated.entities.EntryPoint_AccountDeployed.get(
-      `${event.chainId}_${event.block.number}_${event.logIndex}`
+    let actualUserOperation = mockDbUpdated.entities.UserOperation.get(
+      `${event.chainId}_${event.params.userOpHash}`
     );
 
     // Creating the expected entity
-    const expectedEntryPointAccountDeployed: EntryPoint_AccountDeployed = {
-      id: `${event.chainId}_${event.block.number}_${event.logIndex}`,
+    const expectedUserOperation: UserOperation = {
+      id: `${event.chainId}_${event.params.userOpHash}`,
       userOpHash: event.params.userOpHash,
       sender: event.params.sender,
-      factory: event.params.factory,
       paymaster: event.params.paymaster,
+      nonce: event.params.nonce,
+      success: event.params.success,
+      actualGasCost: event.params.actualGasCost,
+      actualGasUsed: event.params.actualGasUsed,
+      entryPoint: event.srcAddress,
+      transactionHash: event.transaction.hash,
+      chainId: event.chainId,
     };
     // Asserting that the entity in the mock database is the same as the expected entity
-    assert.deepEqual(actualEntryPointAccountDeployed, expectedEntryPointAccountDeployed, "Actual EntryPointAccountDeployed should be the same as the expectedEntryPointAccountDeployed");
+    assert.deepEqual(actualUserOperation, expectedUserOperation, "Actual UserOperation should be the same as the expectedUserOperation");
   });
 });
